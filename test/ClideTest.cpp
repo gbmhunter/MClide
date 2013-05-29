473f8095672d822c86e432ae8b07b90b3e16f778
Geoffrey Hunter <gbmhunter@gmail.com>
2013-05-29 19:30:21 +1200

    Removed unneeded cpp root folder. Added UnitTest++ unit test library.

---
+++ test/ClideTest.cpp
@@ -0,0 +1,19 @@
+
+#include "../src/include/Clide-Cmd.h"
+#include "../src/include/Clide-Param.h"
+#include "../src/include/Clide-Option.h"
+#include "../src/include/Clide-Port.h"
+#include "../src/include/Clide-Tx.h"
+#include "../src/include/Clide-Rx.h"
+
+#include "./UnitTest++/src/UnitTest++.h"
+
+TEST(BasicTest)
+{
+	
+}
+
+int main()
+{
+	return UnitTest::RunAllTests();
+}
